f7abb0f901bda5b37744b0dd13d66276c1c6aba6
Backporter <29647778+Backporter@users.noreply.github.com>
2019-05-12 01:47:09 -0400

    Main
    Main

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # SSE-Fallout-4-Texture-Converter
-This will convert given DDS to gnf
+Check the changelog.MD for more info
+This tool can be used to convert the given ("dds") to GNF
+
+Requirements:
+
+orbis-image2gnf.exe
+
+libSceGnm.dll
+
+libSceGpuAddress.dll
+
+libSceTextureTool.dll
+
+these can be found in the PS4 SDK
+
+location: C:\Program Files (x86)\SCE\ORBIS SDKs\4.500\host_tools\bin
+
+How to install the required files:
+
+step one(1): copy all the required files to the "Data" folder
+
+How to use this tool:
+Get the .dds/.tga files (or folder which contains these files).
+When adding the .dds/.tga to the tool you have a couple of options, you can click the "Add Files" button to add multiple files by hand, you can click "Add Directory..." which will search the directory you choose for .dds/.tga files, or you can drag the folder containing the .dds/.tga files onto the app and it will automatically scan and detect .dds/.tga files.
+Once you have gotten the .dds/.tga files of your choice onto the tool click "Convert" and wait for it to be finished, KEEP IN MIND, With mods with alot of textures and or big textures will take longer.
+Questions and answers:
+
+Q What is this tool for?
+A This tool converters .dds/.tga files to the file format known as .gnf which is used by default in Skyrim Special Edition and Fallout 4 on PS4.
+
+Q What is .gnf, should I use it?
+A .gnf is the format used by default on Skyrim Special Edition and Fallout 4 on PS4. This tool is technically only optional, however it has many advantages. As long as the conversions don't fail (they rarely do for SSE, Fallout 4 is a bit of a mess but slowly being worked on) the .gnf will run better than any .dds/.tga or other texture, it is similar to how a .ddx ran better on PS3 Skyrim than a normal .dds. Another advantage to using .gnf is that you can pack the .gnf textures into .bsa (the textures will load) and organize your mods! No more need for a sloppy data folder or limited folders for textures.
+
+Q This tool just produces another or the same .dds/.tga file, what happened?
+A This tool by default converts the .dds/.tga file to .gnf then renames it back, the reason it does this is because Skyrim Special Edition and Fallout 4 are still looking for the extension .dds/.tga, not .gnf, but keep in mind the game is still using .gnf by default, they are just all renamed to .dds/.tga (except a few exceptions).
+
+Q This tool produced a 0 byte .dds/.tga or didn't do anything to the file, what happened?
+A While rare, the tool can fail to convert certain textures, I am not aware of any fixes, one possible fix could be to process the .dds/.tga again through a texture editor and seeing if that works with the converter. If all fails then it must be a loose file unfortunately.
+
+You are done use the tool as you wish
+
+![Screenshot](MAIN.PNG)
---
+++ SSE-Fallout 4 Texture Converter/Main.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SSE_Fallout_4_Texture_Converter
+{
+    public partial class Main : Form
+    {
+        private List<string> filePaths = new List<string>();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory("DDS");
+            /// thse are what you see when you hover over the buttons
+            toolTip1.SetToolTip(this.btadd, "This is to add them one by one");
+            toolTip1.SetToolTip(this.btadddir, "This is for adding a dirtory");
+            toolTip1.SetToolTip(this.btClear, "this will purge the list");
+            toolTip1.SetToolTip(this.credit, "This will open the credit");
+            toolTip1.SetToolTip(this.btstart, "this will start the ");
+            /// this will check to see if the at9tool is present
+            if (File.Exists("data\\orbis-image2gnf.exe"))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("orbis-image2gnf.exe is missing please put it in the data folder", ("Warning"), MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            /// this will check to see if xWMAEncode.exe is present
+            if (File.Exists("Data\\libSceGnm.dll"))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("libSceGnm.dll is missing please put it in the data folder", ("Warning"), MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            /// this will check to see if "fuz_extractor.exe" is present
+            if (File.Exists("Data\\libSceGpuAddress.dll"))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("libSceGpuAddress.dll is missing please put it in the data folder", ("Warning"), MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            if (File.Exists("Data\\libSceTextureTool.dll"))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("libSceTextureTool.dll is missing please put it in the data folder", ("Warning"), MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            /// this is going to show the Mesagebox that give the warning if anything is missing it might now work like its suppost to
+            MessageBox.Show("if any other required tools are missing this will not work right", ("Warning"), MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            MessageBox.Show("Due to legail reasons i can't include the required tools you will need to find them yourself", ("Warning"), MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void btadd_Click(object sender, EventArgs e)
+        {
+            /// this is going to add files via the add file button to the listbox
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Multiselect = true;
+            OFD.ShowDialog();
+            filePaths.AddRange(OFD.FileNames);
+            for (int i = 0; i < filePaths.Count; i++)
+            {
+                lboxFiles.Items.Add(OFD.SafeFileNames[i]);
+            }
+            /// this is going to tell the system that label is the count of the listbox and the " To be converted "
+            /// IE, "5 To be converted "
+            label1.Text = lboxFiles.Items.Count + " To be converted ";
+        }
+
+        private void btadddir_Click(object sender, EventArgs e)
+        {
+            /// this is going to add all files via the add dir to the listbox
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.Description = "Select the texture Folder";
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                string[] files = Directory.GetFiles(FBD.SelectedPath, "*.dds", SearchOption.AllDirectories);
+                foreach (string dds in files)
+                {
+                    if (!filePaths.Contains(dds))
+                    {
+                        lboxFiles.Items.Add(Path.GetFileName(dds));
+                        filePaths.Add(dds);
+                        label1.Text = lboxFiles.Items.Count + " To be converted ";
+                    }
+                }
+                files = Directory.GetFiles(FBD.SelectedPath, "*.tga", SearchOption.AllDirectories);
+                foreach (string tgafiles in files)
+                {
+                    if (!filePaths.Contains(tgafiles))
+                    {
+                        lboxFiles.Items.Add(Path.GetFileName(tgafiles));
+                        filePaths.Add(tgafiles);
+                        label1.Text = lboxFiles.Items.Count + " To be converted ";
+                    }
+                }
+            }
+            /// this is going to tell the system that label is the count of the listbox and the " To be converted "
+            /// IE, "5 To be converted "
+            label1.Text = lboxFiles.Items.Count + " To be converted ";
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            /// this is going to clear the list box
+            lboxFiles.Items.Clear();
+            /// this is going to refresh the listbox
+            lboxFiles.Refresh();
+            /// this is going to clear the list of file that where in the listbox
+            filePaths.Clear();
+            /// this is going to make the progress bar know how many files there are so it can move the bar acordingly
+            progressBar1.Maximum = lboxFiles.Items.Count;
+            /// this is going to tell the system that label is the count of the listbox and the " To be converted "
+            /// IE, "5 To be converted "
+            label1.Text = lboxFiles.Items.Count + " To be converted ";
+        }
+
+        private void lboxFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] array = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string all in array)
+            {
+                if ((Path.GetExtension(all) == ".dds" || Path.GetExtension(all) == ".tga" || Path.GetExtension(all) == ".DDS" || Path.GetExtension(all) == ".TGA") && !filePaths.Contains(all))
+                {
+                    lboxFiles.Items.Add(Path.GetFileName(all));
+                    filePaths.Add(all);
+                    label1.Text = lboxFiles.Items.Count + " To be converted ";
+                }
+                if (Directory.Exists(all))
+                {
+                    string[] files = Directory.GetFiles(all, "*.dds", SearchOption.AllDirectories);
+                    foreach (string dds in files)
+                    {
+                        if (!filePaths.Contains(dds))
+                        {
+                            lboxFiles.Items.Add(Path.GetFileName(dds));
+                            filePaths.Add(dds);
+                            label1.Text = lboxFiles.Items.Count + " To be converted ";
+                        }
+                    }
+                    files = Directory.GetFiles(all, "*.tga", SearchOption.AllDirectories);
+                    foreach (string tga in files)
+                    {
+                        if (!filePaths.Contains(tga))
+                        {
+                            lboxFiles.Items.Add(Path.GetFileName(tga));
+                            filePaths.Add(tga);
+                            label1.Text = lboxFiles.Items.Count + " To be converted ";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void lboxFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+
+        }
+
+        private void credit_Click(object sender, EventArgs e)
+        {
+            /// this is going to open the credits window
+            if (Application.OpenForms["Credits"] == null)
+            {
+                Credits form = new Credits();
+                form.Show();
+            }
+        }
+
+        private string getFileName(string path)
+        {
+            path = path.Replace("\\", ",");
+            string[] pathSplit = path.Split(',');
+            return pathSplit[pathSplit.Length - 1];
+        }
+
+        private void btstart_Click(object sender, EventArgs e)
+        {
+            /// this is going to create the dirtory dds
+            Directory.CreateDirectory("dds");
+            /// this is going to pass the dirtory info
+            DirectoryInfo info = new DirectoryInfo(Application.StartupPath + "\\dds\\");
+            FileInfo[] files = info.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                file.Delete();
+            }
+            for (int i = 0; i < filePaths.Count; i++)
+            {
+                if (filePaths[i].Contains(".dds"))
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = "Data\\orbis-image2gnf.exe";
+                    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process.StartInfo.Arguments = "-i \"" + filePaths[i] + "\" -o \"" + filePaths[i].ToString().Replace(".dds", ".gnf") + "\" -f auto";
+                    process.Start();
+                    process.WaitForExit();
+                    File.Delete(filePaths[i]);
+                    File.Move(filePaths[i].ToString().Replace(".dds", ".gnf"), (filePaths[i].ToString().Replace(".gnf", ".dds")));
+                    /// this is going to make the progress bar know how many files there are so it can move the bar acordingly
+                    progressBar1.Maximum = lboxFiles.Items.Count;
+                    /// This is going to make it so you can see the bar move
+                    System.GC.Collect();
+                    /// this is going to make it move
+                    progressBar1.Value++;
+                }
+                else if (filePaths[i].Contains(".tga"))
+                {
+                    Process process1 = new Process();
+                    process1.StartInfo.FileName = "Data\\orbis-image2gnf.exe";
+                    process1.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process1.StartInfo.Arguments = "-i \"" + filePaths[i] + "\" -o \"" + filePaths[i].ToString().Replace(".tga", ".tgagnf") + "\" -f auto";
+                    process1.Start();
+                    process1.WaitForExit();
+                    File.Delete(filePaths[i]);
+                    File.Move(filePaths[i].ToString().Replace(".tga", ".tgagnf"), (filePaths[i].ToString().Replace(".tgagnf", ".tga")));
+                    /// this is going to make the progress bar know how many files there are so it can move the bar acordingly
+                    progressBar1.Maximum = lboxFiles.Items.Count;
+                    /// This is going to make it so you can see the bar move
+                    System.GC.Collect();
+                    /// this is going to make it move
+                    progressBar1.Value++;
+                }
+                if (filePaths[i].Contains(".DDS"))
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = "Data\\orbis-image2gnf.exe";
+                    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process.StartInfo.Arguments = "-i \"" + filePaths[i] + "\" -o \"" + filePaths[i].ToString().Replace(".DDS", ".gnf") + "\" -f auto";
+                    process.Start();
+                    process.WaitForExit();
+                    File.Delete(filePaths[i]);
+                    File.Move(filePaths[i].ToString().Replace(".DDS", ".gnf"), (filePaths[i].ToString().Replace(".gnf", ".DDS")));
+                    /// this is going to make the progress bar know how many files there are so it can move the bar acordingly
+                    progressBar1.Maximum = lboxFiles.Items.Count;
+                    /// This is going to make it so you can see the bar move
+                    System.GC.Collect();
+                    /// this is going to make it move
+                    progressBar1.Value++;
+                }
+
+                if (filePaths[i].Contains(".TGA"))
+                {
+                    Process process1 = new Process();
+                    process1.StartInfo.FileName = "Data\\orbis-image2gnf.exe";
+                    process1.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process1.StartInfo.Arguments = "-i \"" + filePaths[i] + "\" -o \"" + filePaths[i].ToString().Replace(".TGA", ".tgagnf") + "\" -f auto";
+                    process1.Start();
+                    process1.WaitForExit();
+                    File.Delete(filePaths[i]);
+                    File.Move(filePaths[i].ToString().Replace(".TGA", ".tgagnf"), (filePaths[i].ToString().Replace(".tgagnf", ".TGA")));
+                    /// this is going to make the progress bar know how many files there are so it can move the bar acordingly
+                    progressBar1.Maximum = lboxFiles.Items.Count;
+                    /// This is going to make it so you can see the bar move
+                    System.GC.Collect();
+                    /// this is going to make it move
+                    progressBar1.Value++;
+                }
+
+                /// this checks to see if they are all done converting
+                if (progressBar1.Value == progressBar1.Maximum)
+                {
+                    /// this is going to show the messege box
+                    MessageBox.Show("Your Textures are converted!", "Finished!");
+                    label1.Text = "Finished";
+                    filePaths.Clear();
+                    lboxFiles.Items.Clear();
+                    progressBar1.Value = 0;
+                }
+            }
+        }
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Directory.Delete("DDS");
+        }
+    }
+}
